7f952fd57d29e9ede896366b0e0a37c5d5e3bba5
Quilograma <50460162+Quilograma@users.noreply.github.com>
2022-11-26 18:21:13 +0000

    api concluded

--- Kafka_consumer/FlaskAPI.py
+++ Kafka_consumer/FlaskAPI.py
@@ -1,13 +1,18 @@
 from flask_httpauth import HTTPDigestAuth
 from Model.models import Visitor
 import json
+from flask import request
 from main import db,app
+import yaml
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'users.yml'), 'r') as f:
+    try:
+        users=yaml.safe_load(f)
+    except yaml.YAMLError as exc:
+        print(exc)
 
 auth = HTTPDigestAuth()
-
-users = {
-    "john": "hello"
-}
 
 @auth.get_password
 def get_pw(username):
@@ -18,9 +23,29 @@ def get_pw(username):
 @app.route('/Visitors', methods=['GET'])
 @auth.login_required
 def get_visitors():
-    list_visitors=Visitor.query.all()
-    results = [obj.to_dict() for obj in list_visitors]
-    return json.dumps(results)
+    args=request.args
+    user_id=args.get('user_id',type=int)
+    page_id=args.get('page_id',type=int)
+
+    if user_id==None and page_id==None:
+        list_visitors=Visitor.query.all()
+        results = [obj.to_dict() for obj in list_visitors]
+        return json.dumps(results)
+    elif user_id!=None and page_id==None:
+        list_visitors=Visitor.get_by_user(user_id)
+        results = [obj.to_dict() for obj in list_visitors]
+        return json.dumps(results)
+    else:
+        list_visitors=Visitor.get_by_page(page_id)
+        results = [obj.to_dict() for obj in list_visitors]
+        return json.dumps(results)
+
+
+@app.route('/Visitors/<int:id>', methods=['GET'])
+@auth.login_required
+def get_visitor_byid(id):
+    visitor=Visitor.get_by_id(id)
+    return json.dumps(visitor.to_dict())
 
 if __name__=='__main__':
     app.run(host='myapp')
